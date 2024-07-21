c07ad5a656ee234195c9f62a09c911002f433666
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-21 13:29:58 +0000

    scripts pour filtre les video long format des video de court format

--- generate_youtube_url/main.py
+++ generate_youtube_url/main.py
@@ -1,30 +1,69 @@
 from googleapiclient.discovery import build
 import time
+import isodate
+
 
 api_key = 'AIzaSyAYaYNBCibmanBU9NHpo2iFNkO7VJhXi58'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 def get_youtube_resutls(query, max_results):
-    results = []
+   
 
-    next_page_token = None
-    while len(results) < max_results:
-        try:
-            request = youtube.search().list(
-                part = 'snippet',
-                q = query,
-                type = 'video',
-                maxResutls = min(max_results - len(results), 50),
-                pageToken = next_page_token
-            )
-            response = request.execute()
-            results.exend(response['items'])
-            if not next_page_token:
-                break
+    
+    request = youtube.search().list(
+        part = 'snippet',
+        q = query,
+        type = 'video',
+        relevanceLanguage = 'fr',
+        order = 'relevance',
+        publishedAfter = '2017-01-01T00:00:00Z',
+        maxResults = max_results,
+        videoDuration = 'long'
+    )
+    response = request.execute()
+    time.sleep(1)
 
-        except Exception as e:
-            print(f"An error occured: {e}")
-            break
+    return response
 
-    return results
+
+def video_response(data):
+    video_id = [item['id']['videoId'] for item in data['items']]
+
+    video_request = youtube.videos().list(
+        part = 'contentDetails, snippet',
+        id = ','.join(video_id)
+    )
+
+    video_response = video_request.execute()
+
+    return video_response
+#Fonction pour converture les duree iso 8601 en secondes
+def fonction_pour_convertire_en_seconde(duration):
+    
+    parsed_duration = isodate.parse_duration(duration)
+
+    return int(parsed_duration.total_seconds())
+#Parcours les résultats et afficher les informations des videos d'au moin de une heure
+def find_resutls(videos):
+    video_response =videos
+
+    for item in video_response['items']:
+        duration = item['contentDetails']['duration']
+        duration_seconds = fonction_pour_convertire_en_seconde(duration)
+
+        if duration_seconds >= 3600: #filtrer les videos d'au moins une heure
+            title = item['snippet']['title']
+            description = item['snippet']['description']
+            thumbnails = item['snippet']['thumbnails']
+            default_thumbnail = thumbnails['default']['url']
+            medium_thumbnail = thumbnails['medium']['url']
+            hight_thumbnail = thumbnails['high']['url']
+
+            print(f'Title: {title}')
+            print(f'Description: {description}')
+            print(f'Duration: {duration}')
+            print(f'meduim thumbnail: {medium_thumbnail}')
+            print(f'default thumbnais: {default_thumbnail}')
+            print(f'high Thumbnails: {hight_thumbnail}')
+
--- video/views.py
+++ video/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Video_long_format, Video_petit_format
-
+from generate_youtube_url.main import get_youtube_resutls ,fonction_pour_convertire_en_seconde, video_response  ,find_resutls
 # Create your views here.
 
+all_video_youtube = get_youtube_resutls("variable en python", 5)
+
+details_video = video_response(all_video_youtube)
 
 
+resulta_final = find_resutls(details_video)
 def index(request):
     return
