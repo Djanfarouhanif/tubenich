b83f3e1547c4fca7c7bf1e912d7c196746e6b438
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-26 14:24:18 +0000

    add logout and login views

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('register', views.register, name='register')
+    path('register', views.register, name='register'),
+    path('login', views.Login, name='login'),
+    path('logout', views.Logout, name='logout')
+
 ]
--- useraccount/views.py
+++ useraccount/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+
 from django.contrib import messages
 
 def register(request):
@@ -19,7 +20,7 @@ def register(request):
             if username and email and langageToLearning:
                 if User.objects.filter(email=email).exists():
                     messages.info(request, 'Email already exists')
-                    return redirect("register")
+                    return redirect("register") 
                 else:
                     new_user = User.objects.create_user(username, email, password)
                     new_user.last_name = lastName
@@ -33,3 +34,21 @@ def register(request):
             return redirect("register")
 
     return HttpResponse('<h1>register page </h1>')
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            user_login = login(request,user)
+        else:
+            messages.info(request, 'username or password is not identifier')
+            return redirect('login')
+    
+    return HttpResponse('<h1>login page</h1>')
+
+def Logout(request):
+    logout(request)
+    
