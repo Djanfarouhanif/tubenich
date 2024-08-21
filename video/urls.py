636439e5827fb2cf2d67370e22c71c2dbcf2daca
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-21 20:58:22 +0000

    create rest_framework_simplejwt.view url

--- video/urls.py
+++ video/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('get_video', views.loader, name='loader'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
 ]
